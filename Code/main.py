b5ecdd7777bb2d2ad45493e9f7389404ac436e1f
Giulio Cardillo <cardillo.giulio@itisgrassi.edu.it>
2026-01-21 11:15:17 +0100

    Updated Gerarchy

---
+++ Code/main.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from time import sleep
+
+# Create a variable to store the start time
+start_time = datetime.now()
+# Create a variable to store the current time
+# (these will be almost the same at the start)
+now_time = datetime.now()
+counter=1
+# Run a loop for 1 minute
+while (now_time < start_time + timedelta(minutes=1)):
+    print("Counter", counter, "Hello ISS now:", now_time, "until:", start_time+ timedelta(minutes=1))
+    counter+=1
+    sleep(10)
+    # Update the current time
+    now_time = datetime.now()
+# Out of the loop — stopping
--- README.md
+++
@@ -1,2 +0,0 @@
-# AstroPi-Mission
-#meow
